f6dbb31e3abbf19d7dd32f5f8c19c04a581bc1d8
Rapin-Thibaud <44433529+Rapin-Thibaud@users.noreply.github.com>
2019-11-15 11:11:40 +0100

    connexion

---
+++ description.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/projectfooter.css" />
+    <link rel="stylesheet" href="css/projectnav.css" />
+    <link rel="stylesheet" href="css/project.css" />
+    <link rel="stylesheet" href="css/description.css" />
+
+    <title>PageLambda</title>
+</head>
+
+<body>
+    <?php include("header.php");
+    ?>
+    <div id="container">
+
+
+        <h1 class="descriptiones">Description</h1>
+        <img src="images/bde.png" class="images" alt="...">
+        <div class="prg">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum rem eveniet obcaecati nesciunt recusandae at animi? Animi fuga illo voluptate ipsum magni omnis. Mollitia quaerat, est iure dolorum qui cumque!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum rem eveniet obcaecati nesciunt recusandae at animi? Animi fuga illo voluptate ipsum magni omnis. Mollitia quaerat, est iure dolorum qui cumque!</p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum rem eveniet obcaecati nesciunt recusandae at animi? Animi fuga illo voluptate ipsum magni omnis. Mollitia quaerat, est iure dolorum qui cumque!</p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum rem eveniet obcaecati nesciunt recusandae at animi? Animi fuga illo voluptate ipsum magni omnis. Mollitia quaerat, est iure dolorum qui cumque!</p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum rem eveniet obcaecati nesciunt recusandae at animi? Animi fuga illo voluptate ipsum magni omnis. Mollitia quaerat, est iure dolorum qui cumque!</p>
+        </div>
+    </div>
+    <a href="#" class="Link_inscription">S'inscrire</a>
+    <footer class="jean">
+
+
+
+        <a href="#">Mentions légales</a>
+        <a href="#">Contactez-nous</a>
+        <a href="#">Menu</a>
+        <a href="#">@Home</a>
+
+    </footer>
+    <?php
+    ?>
+</body>
+
+</html>
---
+++ pageLambda.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/projectfooter.css" />
+  <link rel="stylesheet" href="css/projectnav.css" />
+  <link rel="stylesheet" href="css/project.css" />
+  <link rel="stylesheet" href="css/pageL.css" />
+
+  <title>PageLambda</title>
+</head>
+
+<body>
+
+  <?php include("header.php");
+  ?>
+
+  <div id="container">
+
+
+    <?php include("nav.php");
+    ?>
+    <div class="title">
+      <h1>Liste des associations</h1>
+      <div>
+
+        <div class="big">
+          <div class="card" style="width: 18rem;">
+            <img src="images/bde.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="description/description.php" class="btn btn-primary">Description</a>
+              <a href="formulaireInscription.php" class="btn btn-primary">Inscription</a>
+            </div>
+          </div>
+          <div class="card" style="width: 18rem;">
+            <img src="images/bde.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="description/description.php" class="btn btn-primary">Description</a>
+              <a href="formulaireInscription.php" class="btn btn-primary">Inscription</a>
+            </div>
+          </div>
+          <div class="card" style="width: 18rem;">
+            <img src="images/bde.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="description/description.php" class="btn btn-primary">Description</a>
+              <a href="formulaireInscription.php" class="btn btn-primary">Inscription</a>
+            </div>
+          </div>
+          <div class="card" style="width: 18rem;">
+            <img src="images/bde.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="description/description.php" class="btn btn-primary">Description</a>
+              <a href="formulaireInscription.php" class="btn btn-primary">Inscription</a>
+            </div>
+          </div>
+          <nav class="pages">
+            <nav aria-label="Page navigation example">
+              <ul class="pagination">
+
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="pageLambda2.php">2</a></li>
+                <li class="page-item"><a class="page-link" href="pageLambda3.php">3</a></li>
+
+              </ul>
+            </nav>
+          </nav>
+        </div>
+
+      </div>
+
+
+      <footer class="jean">
+
+
+
+        <a href="#">Mentions légales</a>
+        <a href="#">Contactez-nous</a>
+        <a href="#">Menu</a>
+        <a href="#">@Home</a>
+
+      </footer>
+
+</body>
+
+</html>
