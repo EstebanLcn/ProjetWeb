9e078c55071145fa35d42148f30e0470c86ba75a
Rapin-Thibaud <44433529+Rapin-Thibaud@users.noreply.github.com>
2019-11-17 12:01:15 +0100

    debut css pour le profil

--- connexion/profil.php
+++ connexion/profil.php
@@ -2,16 +2,42 @@
 <?php
 $bdd = new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', '');
 ?>
-<html lang="fr">
+<html>
 
 <head>
     <meta charset="utf-8" />
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/projectfooter.css" />
     <link rel="stylesheet" href="../css/project.css" />
-    <title>Evènements</title>
+    <link rel="icon" href="../images/bde.png" />
+    <link rel="stylesheet" href="../css/profil.css" />
+    <title>Home</title>
 </head>
 
 <body>
+    <header>
+        <div class="high">
+            <a href="../home.php"> <img class="icon" src="../images/bde.png" alt="logo_bde"></a>
+
+            <h1 class="hey">BDE Cesi Bordeaux</h1>
+            <div class="inscription">
+                <?php
+                session_start();
+                if (isset($_SESSION['user_name'])) {
+                    echo '<div> <h5><a href="connexion/profil.php">' . $_SESSION['user_name']  . '</a> |  <a href="connexion/deconnexion.php">Deconnexion</a></h5></div>';
+                } else {
+                    echo '
+                <div >
+                <div> <a href="connexion/mention.php"> S\'inscrire</a>  |  <a href="connexion/connexion.php?var=">Se connecter</a>
+                </div>';
+                }
+                ?>
+            </div>
+        </div>
+    </header>
     <?php
     if (!isset($_SESSION)) {
         session_start();
@@ -48,12 +74,6 @@ $bdd = new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', ''
             $data['id_Role'] = 'personnel CESI';
             break;
     }
-    echo ('<br> le mdp est : ' . $data['password']);
-    echo ('<br> l\'adresse mail : ' . $data['email']);
-    echo (' <br> le role est : ' . $data['id_Role']);
-    echo (' <br> la localisation est : ' . $data['id_Localisation']);
-    echo ('<br><img src=../' . $data['profile_picture'] . '>');
-
     $objet = (object) [
         "email" => $data['email'],
         "passwordd" => $data['password'],
@@ -64,23 +84,24 @@ $bdd = new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', ''
     ];
     $_SESSION['transfert_all'] = $objet; ?>
 
-            <div class="card" style="width: 18rem;">
+            <div class="card cent">
                 <div class="card-body">
-                    <h4 class="card-title"><?php echo ($data['name'] . '    ' . $data['first_name']) ?></h4>
-                    <p class="card-text"><?php echo ($data['email'] . '<br>' . $data['password'] . '<br>' . $data['id_Role'] . '<br>' . $data['id_Localisation'])  ?></p>
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
+                    <h4 class="card-title"><?php echo ($data['name'] . ' ' . $data['first_name']) ?></h4>
+                    <p class="card-text"><?php echo ("adresse mail: " . $data['email'] . '<br>' . "mdp : " . $data['password'] . '<br>' . "role: " . $data['id_Role'] . '<br>' . "centre: " . $data['id_Localisation'])  ?></p>
                 </div>
             </div>
 
             <?php
-            echo ("vous etes: " . ($data['id_Role']));
             if (($data['id_Role']) != "etudiant") {
                 echo ('<a class="btn btn-dark" href="../event/recurrentEvent.php" role="button">Creer un evennement</a>');
-            } else {
-                echo ("nada");
             } ?>
+            <footer class="jean">
+                <a href="mentions_legales">Mentions légales</a>
+                <a href="contact_us">Contactez-nous</a>
+                <a href="#">Menu</a>
+                <a href="home.php">@Home</a>
 
+            </footer>
 </body>
 
 </html>
