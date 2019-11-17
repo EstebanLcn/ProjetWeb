67ded3736b20c6fa0d6f3fcfe691f056abab0c3a
LucasDePaolis <44436162+LucasDePaolis@users.noreply.github.com>
2019-11-17 09:45:52 +0100

    ok

--- procedure/showEvent.sql
+++ procedure/showEvent.sql
@@ -10,13 +10,8 @@ BEGIN
 
 START TRANSACTION;
 
-<<<<<<< HEAD
-SELECT *
-FROM _event
-    INNER JOIN picture ON _event.id = picture.id_event;
-=======
+
 SELECT * FROM _event INNER JOIN picture ON _event.id = picture.id__Event 
->>>>>>> 8ec71c458273226d1f2659752810489f62436281
 
 COMMIT;
 END$$
