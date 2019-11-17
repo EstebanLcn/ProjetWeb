8d3d9c00ce4db56d2c2184187824ff2b2d7349a0
LucasDePaolis <44436162+LucasDePaolis@users.noreply.github.com>
2019-11-17 17:53:55 +0100

    réparation Event

--- Event/addComment.php
+++ Event/addComment.php
@@ -1,17 +1,26 @@
 <?php
 require 'dbh.php';
-$DB=new DB();
+$DB = new DB();
 
 
 echo $_GET['content'];
 echo $_GET['id_event'];
-echo $_GET['id_user'];
+echo $_GET['user_name'];
+
+
+$id_user = $DB->db->prepare('SELECT id FROM _user WHERE first_name=:_user_name');
+$id_user->bindValue(':_user_name', $_GET['user_name'], PDO::PARAM_STR);
+$id_user->execute();
+$id = $id_user->fetchAll();
+var_dump($id[0]['id']);
+
 
 $query = $DB->db->prepare('CALL addComment(:_content,:_id_event,:_id_user)');
-  $query->bindValue(':_content', $_GET['content'], PDO::PARAM_STR);
-  $query->bindValue(':_id_event', $_GET['id_event'], PDO::PARAM_STR);
-  $query->bindValue(':_id_user', $_GET['id_user'], PDO::PARAM_STR);
+$query->bindValue(':_content', $_GET['content'], PDO::PARAM_STR);
+$query->bindValue(':_id_event', $_GET['id_event'], PDO::PARAM_STR);
+$query->bindValue(':_id_user', $id[0]['id'], PDO::PARAM_STR);
 
-  $query->execute();
-  header("Location:recoverEvent.php");
-?>
+$query->execute();
+
+$queri = $query->fetchAll();
+header("Location:recoverEvent.php");
