8aa3bf0101b331053e24f0eb3c445f58d0ad0e11
EstebanLcn <44468643+EstebanLcn@users.noreply.github.com>
2019-11-17 17:20:12 +0100

    Update recoverEvent.php

--- Event/recoverEvent.php
+++ Event/recoverEvent.php
@@ -9,6 +9,7 @@ $query = $DB->db->prepare('CALL showEvent()');
 
 $query->execute();
 $events = $query->fetchAll();
+include('header.php');
 
 //affiche tous les events, remplis avec les données de l'évènement
 
